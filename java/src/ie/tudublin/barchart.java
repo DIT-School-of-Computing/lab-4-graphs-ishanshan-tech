6ae0fe89301ecf370cd05d09888527f250b7615b
Ishan ghai <ghaishan5@gmail.com>
2024-02-14 17:31:21 +0000

    Created java file barchart for exersice 1

---
+++ java/src/ie/tudublin/barchart.java
@@ -0,0 +1,21 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+public class barchart  extends PApplet{
+
+    public void settings()
+	{
+
+    }
+
+
+
+    public void draw()
+	{
+        
+
+    }
+
+
+    
+}
